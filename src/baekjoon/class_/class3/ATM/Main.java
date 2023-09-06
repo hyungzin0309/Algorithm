1eb6fc6f16840e6086b5d6478818894b3f330ce2
hyungjin <hyungzin0309@naver.com>
2023-09-06 18:27:13 +0900

    bj class3

---
+++ src/baekjoon/class_/class3/ATM/Main.java
@@ -0,0 +1,24 @@
+package baekjoon.class_.class3.ATM;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int num = Integer.parseInt(br.readLine());
+        int[] arr = new int[num];
+        String[] s = br.readLine().split(" ");
+        for(int i = 0; i< num; i++){
+            arr[i] = Integer.parseInt(s[i]);
+        }
+        Arrays.sort(arr);
+        int result = 0;
+        for(int i = num; 0 < i; i--){
+            result += arr[num - i] * i;
+        }
+        bw.write(String.valueOf(result));
+        bw.flush();
+    }
+}
